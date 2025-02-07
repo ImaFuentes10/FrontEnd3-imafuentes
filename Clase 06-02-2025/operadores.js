36abfad111ad492a4d7e1aad75d9424a1d0c5083
ImaFuentes10 <ima_fc@hotmail.com>
2025-02-06 19:09:34 -0700

    feat: :sparkles: Agrego operadores lógicos
    Agrego descripción de operadores lógicos y ejemplos

--- Clase 06-02-2025/operadores.js
+++ Clase 06-02-2025/operadores.js
@@ -38,3 +38,23 @@ let diferente = 5 != "5" //Diferente (false)
 let estrictamenteDiferente = 5 !== "5" //Estrictamente diferente (true)
 
 console.log(mayor, menor, igual, estrictamenteIgual, diferente, estrictamenteDiferente)
+
+//Operadores lógicos
+let and = (10 > 5) && (3 < 8) // AND (true)
+let or = (10 > 5) || (3 > 8) // OR (true)
+let not = !(10 > 5) // NOT (false)
+
+console.log(and, or, not);
+
+/*
+    Jerarquía de operadores lógicos
+    1. NOT (!)
+    2. AND (&&)
+    3. OR (||)
+*/
+
+// Ejemplo de jerarquía de operadores lógicos
+var logico1 = true || false && false //Primero evalúo AND = false, luego OR --> true || false = true
+var logico2 = (true || false) && false // (true) && false = false
+
+console.log(logico1, logico2);
