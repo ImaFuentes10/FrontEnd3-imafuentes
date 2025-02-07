f70c7a0955b3c0225712c55ced04764efb354b5a
ImaFuentes10 <ima_fc@hotmail.com>
2025-02-06 18:47:56 -0700

    feat: :sparkles: Agrego operadores de comparación
    Agrego ejemplos de operadores de comparación y comento sus definiciones

--- Clase 06-02-2025/operadores.js
+++ Clase 06-02-2025/operadores.js
@@ -29,3 +29,12 @@ let operacion2 = (10 + 5) * 2 // 15 * 2 = 30
 
 console.log(operacion1, operacion2);
 
+// Operadores de comparación
+let mayor = 10 > 5 //Mayor que
+let menor = 3 < 8 //Menor que
+let igual = 5 == "5" //Comapración de VALOR (true). JS verifica si String puede volverse numero primero
+let estrictamenteIgual = 5 === "5" //Comparación  estricta de VALOR y TIPO DE DATO (false)
+let diferente = 5 != "5" //Diferente (false)
+let estrictamenteDiferente = 5 !== "5" //Estrictamente diferente (true)
+
+console.log(mayor, menor, igual, estrictamenteIgual, diferente, estrictamenteDiferente)
