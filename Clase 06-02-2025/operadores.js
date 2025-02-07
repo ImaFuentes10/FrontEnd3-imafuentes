7b9d0e76063868ffde3886c04baed341c4f5fd1e
ImaFuentes10 <ima_fc@hotmail.com>
2025-02-06 18:41:08 -0700

    feat: :sparkles: Añado ejercicio de operadores
    Operadores matemáticos y pongo a prueba jerarquía de operaciones

---
+++ Clase 06-02-2025/operadores.js
@@ -0,0 +1,31 @@
+/*
+    Operadores de Javascript
+
+    Tipos:-Matemàticos: PAra realizar cálculos.
+*/
+
+// Operadores matemàticos
+
+let suma = 5 + 3
+let resta = 6 - 2
+let multiplicacion = 4 * 7
+let division = 6 / 2 
+let modulo = 10 % 3
+let exponente = 2 ** 3
+
+console.log(suma, resta, multiplicacion, division, modulo, exponente);
+
+/*
+    Jerarquìa de operadores matemàticos
+    1. Paréntesis ()
+    2. Exponentes **
+    3. Multiplicación * y División /
+    4. Suma + y Resta -
+*/
+
+// Ejemplos de jerarquìa
+let operacion1 = 10 + 5 * 2 // 10 + (5 * 2) = 20
+let operacion2 = (10 + 5) * 2 // 15 * 2 = 30
+
+console.log(operacion1, operacion2);
+
