ebec8899ce480a5f9411b908eb322f5ab48a8e83
ImaFuentes10 <ima_fc@hotmail.com>
2025-02-11 19:41:42 -0700

    feat: :sparkles: Añado archivo array.js
    Añado priemros 3 ejercicios de arrays en js

---
+++ Clase 11-02-2025/array.js
@@ -0,0 +1,14 @@
+//Ejemplo 1: Crear un arreglo simple
+
+let frutas = ["Uva", "Manzana", "Pera", "Mango", "Fresa", "Aguacate", "Melón"]
+console.log("Lista de frutas: ", frutas)
+
+//Ejemplo 2: Acceder a elementos de un arreglo
+console.log("Primera fruta: ", frutas [0]);
+console.log("Segunda fruta: ", frutas [1]);
+console.log("Ùltima fruta: ", frutas [5]);
+console.log("Ùltima fruta usado length: ", frutas [frutas.length -1]); //'frutas.length se refiere a la cantidad de elementos que se encuentran dentro del arreglo
+
+//Ejemplo 3: Modificar un elemento del arreglo
+frutas[4] = "Guayaba"
+console.log("Frutas actualizadas", frutas);
