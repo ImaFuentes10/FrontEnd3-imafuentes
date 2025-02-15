848b6903162ef2fd630838d4ea3152e644084b7e
ImaFuentes10 <ima_fc@hotmail.com>
2025-02-15 11:59:41 -0700

    feat: :sparkles: Añado práctica introducción a JS
    Práctica completa

---
+++ Practica-IntroJS/IntroJS.js
@@ -0,0 +1,16 @@
+//Práctica de Introducción a JS
+
+console.log(typeof 42);
+console.log(typeof "Veinticinco");
+console.log(typeof -666);
+console.log(typeof true);
+console.log(typeof 0);
+console.log(typeof "");
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof FALSE);
+console.log(typeof "Imanol");
+console.log(typeof X);
+console.log(typeof 000);
+console.log(typeof +-2);
+console.log(typeof false);
