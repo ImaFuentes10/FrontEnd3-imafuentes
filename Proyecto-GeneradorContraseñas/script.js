8b2728a747e8231bd04bdbb9ce681ad126551113
ImaFuentes10 <ima_fc@hotmail.com>
2025-03-15 16:53:00 -0700

    feat: :sparkles: Añado medidor de seguridad y mejoro código
    Añado funcionalidad al medidor de seguridad y simplifico el código que genera la contraseña
    
    Eliminio redundancias al simplificar el código de generación de contraseñas

--- Proyecto-GeneradorContraseñas/script.js
+++ Proyecto-GeneradorContraseñas/script.js
@@ -14,9 +14,10 @@ const elementoIncluirMayusculas = document.getElementById('incluirMayusculas')
 const elementoIncluirNumeros = document.getElementById('incluirNumeros')
 const elementoIncluirSimbolos = document.getElementById('incluirSimbolos')
 
+//conecto el output en el HTML con el script
 const contraseñaOutput = document.getElementById('contraseñaOutput')
 
-
+//variables que almacenan los arreglos que contienen los códigos ASCII para cada elemento posible
 const MINUS_CHAR_CODES = arrayAscendente(97,122)
 const MAYUS_CHAR_CODES = arrayAscendente(65,90)
 const NUM_CHAR_CODES = arrayAscendente(48,57)
@@ -51,97 +52,78 @@ function sincronizarLongitudCaracteres(e){
     rangoLongitudCaracteres.value = valor
 }
 
+//variables con los elementos que determinan las caracterìsticas de la constraseña
+let longitudCaracteres = numeroLongitudCaracteres.value
+let incluirMinusculas = elementoIncluirMinusculas.checked
+let incluirMayusculas = elementoIncluirMayusculas.checked
+let incluirNumeros = elementoIncluirNumeros.checked
+let incluirSimbolos = elementoIncluirSimbolos.checked
+
+//conecto los cuadrados de seguridad
+const seguridadCuadrado1 = document.getElementById('seguridad-cuadrado-1')
+const seguridadCuadrado2 = document.getElementById('seguridad-cuadrado-2')
+const seguridadCuadrado3 = document.getElementById('seguridad-cuadrado-3')
+const seguridadCuadrado4 = document.getElementById('seguridad-cuadrado-4')
+const colorPrimario = window.getComputedStyle(document.documentElement).getPropertyValue('--color-primario')
+
 //al someter form se generarà la contraseña
 form.addEventListener('submit', e => {
     e.preventDefault()
-
-    //elementos que determinan las caracterìsticas de la constraseña
-    const longitudCaracteres = numeroLongitudCaracteres.value
-    const incluirMinusculas = elementoIncluirMinusculas.checked
-    const incluirMayusculas = elementoIncluirMayusculas.checked
-    const incluirNumeros = elementoIncluirNumeros.checked
-    const incluirSimbolos = elementoIncluirSimbolos.checked
+    
+    //reiniciamos el valor de las variables de los elementos de la contraseña
+    longitudCaracteres = parseInt(numeroLongitudCaracteres.value) //me aseguro de que esta variable sea de tipo number
+    incluirMinusculas = elementoIncluirMinusculas.checked
+    incluirMayusculas = elementoIncluirMayusculas.checked
+    incluirNumeros = elementoIncluirNumeros.checked
+    incluirSimbolos = elementoIncluirSimbolos.checked
 
     //variable de la constraseña tras ser generada
     const contraseña = generarContraseña(longitudCaracteres, incluirMinusculas, incluirMayusculas, incluirNumeros, incluirSimbolos)
-   
+    
     //cambiamos el texto del html por la contraseña generada
     contraseñaOutput.innerText = contraseña
+
+    //console.log(typeof(longitudCaracteres));//Prueba para verificar tipo de variable longitudCaracteres
+
+    //llamamos a función para emdir la seguridad de la contraseña
+    medirSeguridad();
 })
 
-//función para generar la contraseña, toma en cuenta varios elementos ya obtenidos antes
+//función para generar la contraseña, toma en cuenta los elementos ya obtenidos antes
 function generarContraseña(longitudCaracteres, incluirMinusculas, incluirMayusculas, incluirNumeros, incluirSimbolos){
-    //definimos variable para el arreglo de caracteres a crearse dependiendo de los elementos seleccionados
-    let codigoCaracteres = []
-    //decisiones para cada posibilidad de selecciòn de elementos
-    if(incluirMinusculas && (!incluirMayusculas && !incluirNumeros && !incluirSimbolos)){
-        codigoCaracteres = MINUS_CHAR_CODES
-    }
-    if(incluirMayusculas && (!incluirMinusculas && !incluirNumeros && !incluirSimbolos)){
-        codigoCaracteres = MAYUS_CHAR_CODES
-    }
-    if(incluirNumeros && (!incluirMinusculas && !incluirMayusculas && !incluirSimbolos)){
-        codigoCaracteres = NUM_CHAR_CODES
-    }
-    if(incluirSimbolos && (!incluirMinusculas && !incluirMayusculas && !incluirNumeros)){
-        codigoCaracteres = SIM_CHAR_CODES
-    }
-    if(incluirMinusculas && incluirMayusculas && (!incluirNumeros && !incluirSimbolos)){
-        codigoCaracteres = MINUS_CHAR_CODES.concat(MAYUS_CHAR_CODES)
-    }
-    if(incluirMinusculas && incluirNumeros && (!incluirMayusculas && !incluirSimbolos)){
-        codigoCaracteres = MINUS_CHAR_CODES.concat(NUM_CHAR_CODES)
-    }
-    if(incluirMinusculas && incluirSimbolos && (!incluirMayusculas && !incluirNumeros)){
-        codigoCaracteres = MINUS_CHAR_CODES.concat(SIM_CHAR_CODES)
-    }
-    if(incluirMayusculas && incluirNumeros && (!incluirMinusculas && !incluirSimbolos)){
-        codigoCaracteres = MAYUS_CHAR_CODES.concat(NUM_CHAR_CODES)
-    }
-    if(incluirMayusculas && incluirSimbolos && (!incluirMinusculas && !incluirNumeros)){
-        codigoCaracteres = MAYUS_CHAR_CODES.concat(SIM_CHAR_CODES)
-    }
-    if(incluirNumeros && incluirSimbolos && (!incluirMinusculas && !incluirMayusculas)){
-        codigoCaracteres = NUM_CHAR_CODES.concat(SIM_CHAR_CODES)
-    }
-    if(incluirMinusculas && incluirMayusculas && incluirNumeros && !incluirSimbolos){
-        codigoCaracteres = MINUS_CHAR_CODES.concat(MAYUS_CHAR_CODES).concat(NUM_CHAR_CODES)
-    }
-    if(incluirMinusculas && incluirMayusculas && incluirSimbolos && !incluirNumeros){
-        codigoCaracteres = MINUS_CHAR_CODES.concat(MAYUS_CHAR_CODES).concat(SIM_CHAR_CODES)
-    }
-    if(incluirMinusculas && incluirNumeros && incluirSimbolos && !incluirMayusculas){
-        codigoCaracteres = MINUS_CHAR_CODES.concat(MAYUS_CHAR_CODES).concat(SIM_CHAR_CODES)
-    }
-    if(incluirMayusculas && incluirNumeros && incluirSimbolos && !incluirMinusculas){
-        codigoCaracteres = MAYUS_CHAR_CODES.concat(NUM_CHAR_CODES).concat(SIM_CHAR_CODES)
-    }
-    if(incluirMinusculas && incluirMayusculas && incluirNumeros && incluirSimbolos){
-        codigoCaracteres = MINUS_CHAR_CODES.concat(MAYUS_CHAR_CODES).concat(NUM_CHAR_CODES).concat(SIM_CHAR_CODES)
-    }
-
-    //console.log(codigoCaracteres); //prueba impresión código ASCII de los caracteres que conformaràn la contraseña
 
     //declaramos una variable que tiene un arreglo que contendrá a los caracteres seleccionados
     const caracteresContraseña = [];
 
-    //Decisiones para asegurarnos de que haya al menos un caracter del tipo seleccionado
+    // Declaramos una variable que tiene un arreglo que contiene el código ASCII de los tipos de caraqcteres seleccionados
+
+    let codigoCaracteres = [];
+
+    // Agregamos cada tipo de carácter según corresponda
     if (incluirMinusculas) {
+        codigoCaracteres = codigoCaracteres.concat(MINUS_CHAR_CODES);
+        //Nos aseguramos de que haya al menos un caracter del tipo seleccionado, se repite en cada decisión
         const caracterASCII = MINUS_CHAR_CODES[Math.floor(Math.random() * MINUS_CHAR_CODES.length)];
         caracteresContraseña.push(String.fromCharCode(caracterASCII));
     }
+
     if (incluirMayusculas) {
+        codigoCaracteres = codigoCaracteres.concat(MAYUS_CHAR_CODES);
         const caracterASCII = MAYUS_CHAR_CODES[Math.floor(Math.random() * MAYUS_CHAR_CODES.length)];
         caracteresContraseña.push(String.fromCharCode(caracterASCII));
     }
+
     if (incluirNumeros) {
+        codigoCaracteres = codigoCaracteres.concat(NUM_CHAR_CODES);
         const caracterASCII = NUM_CHAR_CODES[Math.floor(Math.random() * NUM_CHAR_CODES.length)];
         caracteresContraseña.push(String.fromCharCode(caracterASCII));
     }
+
     if (incluirSimbolos) {
+        codigoCaracteres = codigoCaracteres.concat(SIM_CHAR_CODES);
         const caracterASCII = SIM_CHAR_CODES[Math.floor(Math.random() * SIM_CHAR_CODES.length)];
         caracteresContraseña.push(String.fromCharCode(caracterASCII));
-    }
+    }  
 
     //ciclo que nos arroja un lugar aleatorio en el arreglo por cada iteración
     //el cual se repite hasta la longitud deseada, partiendo de la cantidad de caracteres que hayamos añadido antes
@@ -176,5 +158,97 @@ function copiarAlPortapapeles(e){
     navigator.clipboard.writeText(contraseñaOutput.innerHTML)
 }
 
+//Medidor de seguridad
+//Basado en tabla 'Time it takes a hacker to brute force your password 2024'
 
 
+
+function medirSeguridad(e){
+
+    let categoria;
+    let valorSeguridad = 0;
+
+    //pruebas lógicas para determinar categorìa de la contraseña
+    if (incluirNumeros && !incluirMinusculas && !incluirMayusculas &&!incluirSimbolos){
+        categoria = 'numOnly';
+    }
+    if ((incluirMinusculas && !incluirMayusculas && !incluirNumeros && !incluirSimbolos) ||
+        (incluirMayusculas && !incluirMinusculas && !incluirNumeros && !incluirSimbolos) ||
+        (incluirSimbolos && !incluirMayusculas && !incluirNumeros && !incluirMinusculas)){
+            categoria = 'oneElement';
+    }
+    if ((incluirMinusculas && incluirMayusculas && !incluirNumeros && !incluirSimbolos) ||
+        (incluirMinusculas && incluirNumeros && !incluirMayusculas && !incluirSimbolos) ||
+        (incluirMinusculas && incluirSimbolos && !incluirNumeros && !incluirMayusculas) ||
+        (incluirMayusculas && incluirNumeros && !incluirMinusculas && !incluirSimbolos) ||
+        (incluirMayusculas && incluirSimbolos && !incluirMinusculas && !incluirNumeros) ||
+        (incluirNumeros && incluirSimbolos && !incluirMinusculas && !incluirMayusculas)){
+            categoria = 'twoElements';
+    }
+    if ((incluirMinusculas && incluirMayusculas && incluirNumeros && !incluirSimbolos) ||
+        (incluirMinusculas && incluirMayusculas && incluirSimbolos && !incluirNumeros) ||
+        (incluirMinusculas && incluirNumeros && incluirSimbolos && !incluirMayusculas) ||
+        (incluirMayusculas && incluirNumeros && incluirSimbolos&& !incluirMinusculas)){
+            categoria = 'threeElements';
+    }
+    if(incluirMinusculas && incluirMayusculas && incluirNumeros && incluirSimbolos){
+        categoria = 'fourElements';
+    }
+    
+    //pruebas de comparación con longitudCaracteres para determinar valorSeguridad de la contraseña
+    if (categoria === 'numOnly' && longitudCaracteres < 15 ||
+        categoria === 'oneElement' && longitudCaracteres < 10 ||
+        categoria === 'twoElements' && longitudCaracteres < 9 ||
+        categoria === 'threeElements' && longitudCaracteres < 8 ||
+        categoria === 'fourElements' && longitudCaracteres < 8){
+            valorSeguridad = 0;
+    }
+    if (categoria === 'numOnly' && longitudCaracteres >= 15 ||
+        categoria === 'oneElement' && longitudCaracteres >= 10 && longitudCaracteres < 14 ||
+        categoria === 'twoElements' && longitudCaracteres >= 9 && longitudCaracteres < 12 ||
+        categoria === 'threeElements' && longitudCaracteres >= 8 && longitudCaracteres < 11 || 
+        categoria === 'fourElements' && longitudCaracteres >= 8 && longitudCaracteres < 11){
+        valorSeguridad = 1;
+    }
+    if (categoria === 'oneElement' && longitudCaracteres >= 14 && longitudCaracteres < 17 ||
+        categoria === 'twoElements' && longitudCaracteres >= 12 && longitudCaracteres < 14 ||
+        categoria === 'threeElements' && longitudCaracteres >= 11 && longitudCaracteres < 14 ||
+        categoria === 'fourElements' && longitudCaracteres >=11 && longitudCaracteres < 13){
+            valorSeguridad = 2;
+    }
+    if (categoria === 'oneElement' && longitudCaracteres >= 17 ||
+        categoria === 'twoElements' && longitudCaracteres >= 14 || 
+        categoria === 'threeElements' && longitudCaracteres >= 14 ||
+        categoria === 'fourElements' && longitudCaracteres >=13){
+            valorSeguridad = 3;
+        }
+        
+        console.log(categoria);
+        console.log(valorSeguridad);
+
+    if(valorSeguridad === 0){
+        seguridadCuadrado1.style.backgroundColor = "red";
+        seguridadCuadrado2.style.backgroundColor = colorPrimario;
+        seguridadCuadrado3.style.backgroundColor = colorPrimario;
+        seguridadCuadrado4.style.backgroundColor = colorPrimario;
+    }
+    
+    if(valorSeguridad === 1){
+        seguridadCuadrado1.style.backgroundColor = 'orange';
+        seguridadCuadrado2.style.backgroundColor = 'orange';
+        seguridadCuadrado3.style.backgroundColor = colorPrimario;
+        seguridadCuadrado4.style.backgroundColor = colorPrimario;
+    }
+    if(valorSeguridad === 2){
+        seguridadCuadrado1.style.backgroundColor = 'yellow';
+        seguridadCuadrado2.style.backgroundColor = 'yellow';
+        seguridadCuadrado3.style.backgroundColor = 'yellow';
+        seguridadCuadrado4.style.backgroundColor = colorPrimario;
+    }
+    if(valorSeguridad === 3){
+        seguridadCuadrado1.style.backgroundColor = 'green';
+        seguridadCuadrado2.style.backgroundColor = 'green';
+        seguridadCuadrado3.style.backgroundColor = 'green';
+        seguridadCuadrado4.style.backgroundColor = 'green';
+    }
+}
